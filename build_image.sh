df96b6ff3bee13cd47292155a1d2aebb3c8ba81f
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-03-16 15:21:44 +0400

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -6,6 +6,6 @@
 # Copy our config folder
 cp out/linux_arm_v7/client pigen-splitstreamer/01-copy-binaries
 cp pigen.config pi-gen/config
-cp -r pigen-splitstreamer pi-gen/
+cp -r pigen-splitstreamer pi-gen/splitstreamer
 cd pi-gen
 ./build-docker.sh
