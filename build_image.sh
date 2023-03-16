1e0becacd83a93891cd894f464f77e6cdb66a37a
MatthieuCoder <matthieu@matthieu-dev.xyz>
2023-03-16 06:44:28 +0000

    add pi image

---
+++ build_image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# build our binaries
+docker buildx bake --set "*.platform=linux/arm/v7" --set "*.target=bare" --set "*.output=type=local,dest=out"
+
+# Copy our config folder
+cp out/linux_arm_v7/client pigen-splitstreamer/01-copy-binaries
+cp pigen.config pi-gen/config
+cp -r pigen-splitstreamer pi-gen/
+cd pi-gen
+./build.sh
