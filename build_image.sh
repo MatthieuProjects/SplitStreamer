f9089a5fa6f96869f60e27234bb0c96b48341d99
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-03-16 12:08:59 +0400

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # build our binaries
-docker buildx bake --set "*.platform=linux/arm/v7" --set "*.target=bare" --set "*.output=type=local,dest=out"
+#docker buildx bake --set "*.platform=linux/arm/v7" --set "*.target=bare" --set "*.output=type=local,dest=out"
 
 # Copy our config folder
 cp out/linux_arm_v7/client pigen-splitstreamer/01-copy-binaries
